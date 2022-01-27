40d5aeba055671ff29156a66348ff4734cc7be84
iachichurov <a.iachichurov@gmail.com>
2022-01-27 19:09:53 +1000

    first commit

---
+++ hello.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+main () {
+  printf("Hello, my dear waifu\nWith love, Aleks\n");
+}
---
+++ temperature.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*Вывод таблиц температуры по Фаренгейту и Цельсию от 0 до 300 F*/
+
+int main() {
+  int fahr, celcius;
+  int lower, upper, step;
+
+  lower = 0; /*нижняя граница температуры*/
+  upper = 300; /*верхняя граница температуры*/
+  step = 20; /*шаг изменения*/
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celcius = 5 * (fahr - 32) / 9;
+    printf ("%d\t%d\n", fahr, celcius);
+    fahr = fahr + step;
+  }
+}
