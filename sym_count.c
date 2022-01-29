c5af2220105550ca2450b2a513f770f5d09b74f8
iachichurov <a.iachichurov@gmail.com>
2022-01-30 05:12:12 +1000

    Version 2.0

--- sym_count.c
+++ sym_count.c
@@ -1,15 +1,21 @@
 /*Подсчет символов во входном потоке*/
 #include <stdio.h>
 
-/*Первая версия*/
-
 main()
 {
+  /*Первая версия
   long nc;
   nc = 0;
 
   while (getchar() != EOF) {
     nc++;
     printf("%ld\n", nc);
-  }
+  }*/
+
+  /*Вторая версия*/
+  double nc;
+
+  for (nc = 0; getchar() != EOF; nc++)
+  ;
+  printf("%.0f\n", nc);
 }
